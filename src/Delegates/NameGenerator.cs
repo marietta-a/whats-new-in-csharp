111b75a9dfc07ad852d5f46bbf5e4853d8c7cb08
akumbom <akumbom5ma@gmail.com>
2026-04-30 12:07:37 +0100

    Add delegates

---
+++ src/Delegates/NameGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Csharp.Delegates
+{
+    public class NameGenerator
+    {
+        /// <summary>
+        /// Delegate that represents a function which composes a full name
+        /// from a first name and a last name.
+        /// </summary>
+        /// <param name="firstName">The given first name.</param>
+        /// <param name="lastName">The given last name.</param>
+        /// <returns>The composed full name.</returns>
+        public delegate string NameDelegate(string firstName, string lastName);
+
+        /// <summary>
+        /// Default formatter that concatenates first and last name with a space.
+        /// Kept private because it's an internal implementation detail.
+        /// </summary>
+        private static string FullName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+
+        /// <summary>
+        /// A handler instance initialized with the default <see cref="FullName"/> formatter.
+        /// This can be reassigned to other <see cref="NameDelegate"/> implementations.
+        /// </summary>
+        public NameDelegate handler = FullName;
+
+        /// <summary>
+        /// Invokes the provided <see cref="NameDelegate"/> to generate a name and
+        /// writes the result to the console.
+        /// </summary>
+        /// <param name="nameDelegate">The delegate used to compose the name.</param>
+        /// <param name="firstName">The first name to use.</param>
+        /// <param name="lastName">The last name to use.</param>
+        public void GenerateName(NameDelegate nameDelegate, string firstName, string lastName)
+        {
+            // Invoke the delegate to produce the full name and print it.
+            var fullName = nameDelegate(firstName, lastName);
+            Console.WriteLine(fullName);
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,21 @@
-﻿using Csharp15;
+﻿using Csharp.Delegates;
+using Csharp15;
 
 
 /// Sum elements in the same index of two array sequences, 
 /// if one sequence is shorter, the missing elements are replaced with default value of the element type (0 for int).
 
-var functions = new MyBenchMarks();
+//var functions = new MyBenchMarks();
 
-Console.WriteLine($"Array Sum: {string.Join(", ", functions.BenchmarkArraySum())}");
-Console.WriteLine($"Extension Method With Delegate Sum: {string.Join(", ", functions.BenchmarkExtensionMethodWithDelegateSum())}");
+//Console.WriteLine($"Array Sum: {string.Join(", ", functions.BenchmarkArraySum())}");
+//Console.WriteLine($"Extension Method With Delegate Sum: {string.Join(", ", functions.BenchmarkExtensionMethodWithDelegateSum())}");
 //BenchmarkDotNet.Running.BenchmarkRunner.Run<MyBenchMarks>();
+
+
+
+/// Create a delegate that takes two strings (first name and last name) and returns a full name.
+var nameGenerator = new NameGenerator();
+nameGenerator.GenerateName(nameGenerator.handler, "Jane", "Doe");
+nameGenerator.GenerateName(nameGenerator.handler, "Paul", "Doe");
+nameGenerator.GenerateName(nameGenerator.handler, "Mum", "Doe");
+nameGenerator.GenerateName(nameGenerator.handler, "Dad", "Doe");
