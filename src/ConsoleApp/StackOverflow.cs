2f166998634fe450b63ef63ee88ba48f0a0d52d0
akumbom <akumbom5ma@gmail.com>
2026-05-15 09:02:58 +0100

    Add project for stackoverflow questions

---
+++ src/ConsoleApp/StackOverflow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class StackOverflow
+    {
+        public void CheckSession(Dictionary<string, object?> session)
+        {
+
+            string y = session["key"]?.ToString() ?? "none";
+
+            Console.WriteLine(y);
+
+        }
+    }
+}
---
+++ src/Csharp14/StackOverflow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Csharp.Csharp14
+{
+    public class StackOverflow
+    {
+        public List<object> dataList;
+        public bool AddData(ref object data) { 
+            bool success = false;
+            try
+            {
+                // I've also used "if (data != null)" which hasn't worked either
+                dataList?.Add(data);
+                success = DoOtherStuff(data);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.ToString());
+            }
+            return success;
+        }
+
+        public bool DoOtherStuff(object data) {
+            return true;
+        }
+
+    }
+
+}
